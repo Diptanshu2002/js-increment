1d16a0e5b23f0ba28ade5b2f0c53d925e9035818
Diptanshu Mandal <diptanshu2002@outlook.com>
2021-08-05 15:02:05 +0530

    first commit-js

---
+++ demo/index.js
@@ -0,0 +1,9 @@
+let count = 0;
+//let counterText = document.getElementById("count");
+console.log(counterText);
+function increment() {
+  count++;
+  //counterText.innerText = count;
+  document.getElementById("count").innerText = count;
+  console.log(count);
+}

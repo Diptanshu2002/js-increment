9d259211a834b8985ec46658661beabec7792526
Diptanshu Mandal <diptanshu2002@outlook.com>
2021-08-06 00:17:02 +0530

    Added Save-btn and Storing previous entries

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,11 @@
 let count = 0;
-//let counterText = document.getElementById("count");
-console.log(counterText);
 function increment() {
   count++;
-  //counterText.innerText = count;
   document.getElementById("count").innerText = count;
   console.log(count);
+}
+function saveFunc() {
+  let saveEl = document.getElementById("savePreviousData");
+  saveEl.innerText = `${count} - `;
+  console.log(saveEl);
 }
